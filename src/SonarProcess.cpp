3d1877ba9035f5219d8cb0a6276679d4bd250387
jinholee <jinholee@student.42seoul.kr>
2024-05-30 23:10:14 +0200

    updated sonar process

--- src/SonarProcess.cpp
+++ src/SonarProcess.cpp
@@ -5,12 +5,16 @@ SonarProcess::SonarProcess() {}
 SonarProcess::~SonarProcess() {}
 
 SonarData SonarProcess::process(const std::vector<uint8_t>& data) {
-    if (data.size() < 6){
-        return {0, 0, 0};
+    if (data.size() < 4){
+        return {100, 100};
     }
-    uint32_t frontSensorLeft = static_cast<unsigned int> ((data[0] << 8) | data[1]);
-    uint32_t frontSensorMiddle = static_cast<unsigned int> ((data[2] << 8) | data[3]);
-    uint32_t frontSensorRight = static_cast<unsigned int> ((data[4] << 8) | data[5]);
+    uint32_t frontSensor = static_cast<unsigned int> ((data[0] << 8) | data[1]);
+    uint32_t rearSensor = static_cast<unsigned int> ((data[2] << 8) | data[3]);
 
-    return {frontSensorLeft, frontSensorMiddle, frontSensorRight};
+    if (frontSensor == 0)
+        frontSensor = 100;
+    if (rearSensor == 0)
+        rearSensor = 100;
+
+    return {frontSensor, rearSensor};
 }
