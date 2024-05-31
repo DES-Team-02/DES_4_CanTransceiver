ff3f0e2a9011b24f2a221a955000faa02e7a3f7e
jinholee <jinholee@student.42seoul.kr>
2024-05-31 20:25:40 +0200

    socketCAN with epoll

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,111 @@
-# include "CanReceiver.hpp"
-# include "RpmProcess.hpp"
-# include "SonarProcess.hpp"
-# include "CanTransceiverStubImpl.hpp"
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <linux/can.h>
+#include "CanTransceiverStubImpl.hpp"
 
-#define SPEED_SENSOR_CAN_ID 100
-#define SONAR_SENSOR_CAN_ID 200
+#define NUMBER_OF_CAN_INTERFACES 2
 
-void processData(struct can_frame frame, std::shared_ptr<CanTransceiverStubImpl> transceiverStubImpl)
+int create_and_bind_socket(const std::string& interface)
 {
-    std::vector<uint8_t> buffer = std::vector<uint8_t>(frame.data, frame.data + frame.can_dlc);
-    switch (frame.can_id)
+    struct ifreq ifr;           // Interface request structure for socket ioctls
+    struct sockaddr_can addr;   // Address structure for the CAN socket
+
+    int soc = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (soc < 0) 
     {
-    case SPEED_SENSOR_CAN_ID:
+        std::cerr << "Socket Creation Error!" << std::endl;
+        return -1;
+    }
+    addr.can_family = AF_CAN;
+    strcpy(ifr.ifr_name, interface.c_str());
+
+    if (ioctl(soc, SIOCGIFINDEX, &ifr) < 0) 
     {
-        auto processedRpmData = RpmProcess::process(buffer);
-        transceiverStubImpl->setRpmAttribute(processedRpmData.rpm);
-        transceiverStubImpl->setSpeedAttribute(processedRpmData.speed);
-        break;
+        std::cerr << "I/O Control Error." << std::endl;
+        // std::cerr << errno << std::endl;
+        close(soc);
+        return (-1);
     }
-    case SONAR_SENSOR_CAN_ID:
+    addr.can_ifindex = ifr.ifr_ifindex;
+    fcntl(soc, F_SETFL, O_NONBLOCK);
+
+    if (bind(soc, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
     {
-        auto processedSonarData = SonarProcess::process(buffer);
-        transceiverStubImpl->setDistancesAttribute(processedSonarData);
-        break;
+        std::cerr << "Binding Error." << std::endl;
+        close(soc);
+        return -1;
     }
-    default:
-        std::cerr << "Unknown CAN message ID: " << frame.can_id << std::endl;
-        break;
-    }
+    return soc;
 }
 
 int main(void)
 {
-    // Create and start CAN receivers for two interfaces.
-    CanReceiver             can0("can0");
-    CanReceiver             can1("can1");
+    int epoll_fd, nfds;
+    struct epoll_event event, events[NUMBER_OF_CAN_INTERFACES];
 
-    auto transceiverStubImpl = std::make_shared<CanTransceiverStubImpl>();
-
-    transceiverStubImpl->init();
-    // Start receiving data.
-    can0.start();
-    can1.start();
-    const double wheel_radius = 0.065;
-    const double disk_radius  = 0.025;
-
-    // Example data receive and process loop.
-    // shouldContinue() defines the condition to keep running.
-    while (true)
+    // Create Epoll Instance
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) 
     {
-        // auto rpmData    = can0.getReceivedData();
-        // printData("rpmData in main", rpmData);
-        // auto sonarData  = can1.getReceivedData();
-        // printData("sonarData in main", sonarData);
-
-        struct can_frame frame0 = can0.getReceivedData();
-        struct can_frame frame1 = can1.getReceivedData();
-
-        processData(frame0, transceiverStubImpl);
-        processData(frame1, transceiverStubImpl);
-
-        // // Process each set of data.
-        // auto processedRpmData   = RpmProcess::process(rpmData);
-        // auto processedSonarData = SonarProcess::process(sonarData);
-
-        // // Register processed data.
-        // transceiverStubImpl->setRpmAttribute(processedRpmData.rpm);
-        // transceiverStubImpl->setSpeedAttribute(processedRpmData.speed);
-
-        // transceiverStubImpl->setDistancesAttribute(processedSonarData);
-
-        // std::cout << "Filtered RPM: " << processedRpmData.rpm << ", Speed: " << processedRpmData.speed << std::endl;
-        // std::cout << "Front Sensor Left: " << processedSonarData.getSensorfrontleft()
-        //   << ", Front Sensor Middle: " << processedSonarData.getSensorfrontmiddle()
-        //   << ", Front Sensor Right: " << processedSonarData.getSensorfrontright() << std::endl;
+        std::cerr << "epoll_create1 failed" << std::endl;
+        return 1;
     }
 
-    // Stop receiving data.
-    can0.stop();
-    can1.stop();
+    int can0 = create_and_bind_socket("can0");
+    if (can0 < 0)
+    {
+        close(epoll_fd);
+        return 1;
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = can0;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, can0, &event) == -1) {
+        std::cerr << "epoll_ctl failed for can0" << std::endl;
+        close(can0);
+        close(epoll_fd);
+        return 1;
+    }
+
+    while (true) {
+        nfds = epoll_wait(epoll_fd, events, NUMBER_OF_CAN_INTERFACES, -1);
+        if (nfds == -1) 
+        {
+            std::cerr << "epoll_wait failed" << std::endl;
+            break;
+        }
+
+        for (int n = 0; n < nfds; ++n) {
+            if (events[n].events & EPOLLIN) 
+            {
+                struct can_frame frame;
+                ssize_t nbytes = recv(soc, &frame, sizeof(struct can_frame), 0);
+                switch (frame.can_id)
+                {
+                case 0x100:
+                {
+                    std::cout << "speed" << std::endl;
+                    break;
+                }
+                case 0x200:
+                {
+                    std::cout << "sonar" << std::endl;
+                    break;
+                }
+                default:
+                    std::cout << "Unknown CAN ID" << std::endl;
+                    break;
+                }
+            }
+        }
+    }
 
     return 0;
 }
